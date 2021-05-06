d34e4e5dada003f02c988eb5ce6db04fc3a43f84
Matveev Andrey <andyzinger@gmail.com>
2021-05-06 12:51:10 +0300

    поправлен пример конфиг-файла

--- src/config.example.php
+++ src/config.example.php
@@ -1,1 +1,12 @@
 <?php
+return [
+    'api_url' => 'https://test.mgc-loyalty.ru/',
+    'login' => '',
+    'password' => '',
+    'transaction_start_id' => 0,
+    'db_host' => 'mysql',
+    'db_port' => '3306',
+    'db_user_name' => 'root',
+    'db_password' => '',
+    'db_name' => 'test',
+];
